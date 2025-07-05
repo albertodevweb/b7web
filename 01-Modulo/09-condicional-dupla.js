12b3e1deea79485d0a98fa8e63b0a64d22206ae5
albertodevweb <albertodevweb@gmail.com>
2025-07-04 21:03:25 -0300

    condicional dupla

---
+++ 01-Modulo/09-condicional-dupla.js
@@ -0,0 +1,10 @@
+let idade = 19;
+if (idade < 18) {
+  console.log("você é uma criança.");
+} else if (idade >= 18 && idade < 60) {
+  console.log("você é uma adulto.");
+} else if (idade >= 60) {
+  console.log("você é uma idoso.");
+} else {
+  console.log("você tá é morto kkkk.");
+}
